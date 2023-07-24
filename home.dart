6dea69fb29a7c46802ffa9355122e8fcf1ce7932
Halil Can Parlayan <69989315+CanParlayan@users.noreply.github.com>
2023-07-24 12:22:53 +0300

    both api's implemented

--- home.dart
+++ home.dart
@@ -63,60 +63,87 @@ class _HomeState extends State<Home> {
       });
       // Check if teeth are detected in the image
       bool containsTeeth =
-          _output.any((prediction) => prediction.tagName == 'teeth');
+      _output.any((prediction) => prediction.tagName == 'teeth');
 
       if (!containsTeeth) {
         // If teeth are not detected, show a message
         showDialog(
           context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('No teeth detected'),
-            content: const Text('Please take a photo of your teeth.'),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text('OK'),
+          builder: (context) =>
+              AlertDialog(
+                title: const Text('No teeth detected'),
+                content: const Text('Please take a photo of your teeth.'),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text('OK'),
+                  ),
+                ],
               ),
-            ],
-          ),
         );
       } else {
-        // If teeth are detected, check for cavities
-        bool hasCavity =
-            _output.any((prediction) => prediction.tagName == 'cavity');
+        // If teeth are detected, proceed to check for cavities using another API
 
-        // Show the result to the user
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('Teeth Classification Result'),
-            content: Text(hasCavity
-                ? 'Your teeth have a cavity.'
-                : 'Your teeth are cavity-free.'),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text('OK'),
-              ),
-            ],
-          ),
+        // Replace the following constant and headers with the ones for the cavity detection API
+        const cavityUrl = 'https://dentalprediction-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/b874109f-ffeb-428f-a30b-a9db47b75f26/classify/iterations/Iteration1/image';
+        final cavityHeaders = {
+          'Prediction-Key': '6c41e9fb16f64bf39c334fb6ae1761bc',
+          'Content-Type': 'application/octet-stream',
+        };
+
+        var cavityBytes = await image.readAsBytes();
+        var cavityResponse = await http.post(
+          Uri.parse(cavityUrl),
+          headers: cavityHeaders,
+          body: cavityBytes,
         );
+
+        if (cavityResponse.statusCode == 200) {
+          if (kDebugMode) {
+            print(cavityResponse.body);
+          }
+
+          var cavityData = jsonDecode(cavityResponse.body);
+          bool hasCavity = cavityData['hasCavity'] ?? false;
+
+          // Show the result to the user
+          showDialog(
+            context: context,
+            builder: (context) =>
+                AlertDialog(
+                  title: const Text('Teeth Classification Result'),
+                  content: Text(hasCavity
+                      ? 'Your teeth have a cavity.'
+                      : 'Your teeth are cavity-free.'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text('OK'),
+                    ),
+                  ],
+                ),
+          );
+        } else {
+          // Handle error from the cavity detection API here
+          if (kDebugMode) {
+            print('Failed to check for cavity with status ${cavityResponse
+                .statusCode}');
+          }
+        }
       }
     } else {
-      // Handle error here
+      // Handle error from the teeth detection API here
       if (kDebugMode) {
-        print('Failed with status ${response.statusCode}');
+        print('Failed to detect teeth with status ${response.statusCode}');
       }
-      setState(() {
-        _loading = false;
-      });
     }
   }
 
   Future<void> pickImage(ImageSource imageSource) async {
     var image = await picker.pickImage(source: imageSource);
-    if (image == null)
+    if (image == null) {
       return; // Handle the case when the user cancels image selection
+    }
 
     setState(() {
       _image = File(image.path);
@@ -136,8 +163,6 @@ class _HomeState extends State<Home> {
       });
     });
   }
-
-  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -170,48 +195,49 @@ class _HomeState extends State<Home> {
                 child: _loading
                     ? const CircularProgressIndicator()
                     : _image != null
-                        ? Column(
-                            children: [
-                              SizedBox(
-                                height: MediaQuery.of(context).size.width * 0.5,
-                                width: MediaQuery.of(context).size.width * 0.5,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(30),
-                                  child: Image.file(
-                                    _image!,
-                                    fit: BoxFit.fill,
-                                  ),
-                                ),
-                              ),
-                              const Divider(
-                                height: 25,
-                                thickness: 1,
-                              ),
-                              // ignore: unnecessary_null_comparison
-                              if (_output != null && _output.isNotEmpty)
-                                Text(
-                                  'The teeth has : ${_output.isNotEmpty ? _output[1].tagName : ""}',
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              const Divider(
-                                height: 25,
-                                thickness: 1,
-                              ),
-                            ],
-                          )
-                        : const Text(
-                            'No image selected'), // Show "No image selected" message
+                    ? Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery
+                          .of(context)
+                          .size
+                          .width * 0.5,
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width * 0.5,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image.file(
+                          _image!,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    const Divider(
+                      height: 25,
+                      thickness: 1,
+                    ),
+                    // ignore: unnecessary_null_comparison
+                    if (_output != null && _output.isNotEmpty)
+                      const Divider(
+                        height: 25,
+                        thickness: 1,
+                      ),
+                  ],
+                )
+                    : const Text(
+                    'No image selected'), // Show "No image selected" message
               ),
               Column(
                 children: [
                   GestureDetector(
                     onTap: () => pickImage(ImageSource.camera),
                     child: Container(
-                      width: MediaQuery.of(context).size.width - 200,
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width - 200,
                       alignment: Alignment.center,
                       padding: const EdgeInsets.symmetric(
                           horizontal: 24, vertical: 17),
@@ -227,10 +253,15 @@ class _HomeState extends State<Home> {
                   ),
                   const SizedBox(height: 30),
                   GestureDetector(
-                    onTap: () => pickImage(ImageSource
-                        .gallery), // Call the new function for gallery image selection
+                    onTap: () =>
+                        pickImage(ImageSource
+                            .gallery),
+                    // Call the new function for gallery image selection
                     child: Container(
-                      width: MediaQuery.of(context).size.width - 200,
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width - 200,
                       alignment: Alignment.center,
                       padding: const EdgeInsets.symmetric(
                           horizontal: 24, vertical: 17),
