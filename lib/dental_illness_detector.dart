3b594b87580cd84a983c997e3171c6d7ccdf3e8d
Halil Can Parlayan <69989315+CanParlayan@users.noreply.github.com>
2023-08-02 15:01:59 +0300

    Yeni api'a göre olasılık değerleri değişti

--- lib/dental_illness_detector.dart
+++ lib/dental_illness_detector.dart
@@ -10,23 +10,23 @@ class DentalIllnessDetector {
 
   void detectIllnesses(Map<String, String> illnessTranslations) {
     bool isHealthy = dentalIllnessData.entries
-        .any((entry) => entry.key == 'healthy' && entry.value > 0.75);
+        .any((entry) => entry.key == 'healthy' && entry.value > 0.47);
     if (isHealthy) {
       _dialogResultText = "healthyRes".tr;
     } else {
       bool hasMouthUlcer = dentalIllnessData.entries
-          .any((entry) => entry.key == 'mouth ulcer' && entry.value > 0.48);
+          .any((entry) => entry.key == 'mouth ulcer' && entry.value > 0.44);
       if (hasMouthUlcer) {
         detectedIllnesses.add("ulcerRes".tr);
       }
 
       bool hasToothDecay = dentalIllnessData.entries
-          .any((entry) => entry.key == 'tooth decay' && entry.value > 0.24);
+          .any((entry) => entry.key == 'tooth decay' && entry.value > 0.44);
       if (hasToothDecay) {
         detectedIllnesses.add("decayRes".tr);
       }
       bool hasCalculus = dentalIllnessData.entries
-          .any((entry) => entry.key == 'calculus' && entry.value > 0.47);
+          .any((entry) => entry.key == 'calculus' && entry.value > 0.38);
       if (hasCalculus) {
         detectedIllnesses.add("calculusRes".tr);
       }
